a4dd6ffe5f7fed71266eac628dc5235c17efe9d7
Ryan Alsobrooks <alsobrooks.ryan@gmail.com>
2024-09-29 09:16:37 -0400

    Standardize headers

--- app/flashcard/page.tsx
+++ app/flashcard/page.tsx
@@ -135,13 +135,13 @@ const FlashcardApp: React.FC = () => {
             {/* Section header */}
             <div className="mx-auto max-w-3xl pb-12 text-center md:pb-20">
               <div className="inline-flex items-center gap-3 pb-3 before:h-px before:w-8 before:bg-gradient-to-r before:from-transparent before:to-indigo-200/50 after:h-px after:w-8 after:bg-gradient-to-l after:from-transparent after:to-indigo-200/50">
-                <span className="inline-flex bg-gradient-to-r from-indigo-500 to-indigo-200 bg-clip-text text-transparent">
+                <span className="inline-flex bg-gradient-to-r from-indigo-500 to-indigo-200 bg-clip-text text-transparent text-3xl">
                   Flashcard Quiz
                 </span>
               </div>
-              <h2 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-4 font-nacelle text-3xl font-semibold text-transparent md:text-4xl">
+              <h1 className="text-5xl animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-4 font-nacelle font-semibold text-transparent">
                 Test Your Knowledge
-              </h2>
+              </h1>
             </div>
 
             {/* Input form */}
