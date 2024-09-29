7dce0cd535f1b43faaee4199d779f06fb21ce46a
Ryan Alsobrooks <alsobrooks.ryan@gmail.com>
2024-09-28 23:34:32 -0400

    very good styling

--- app/notetaker/page.tsx
+++ app/notetaker/page.tsx
@@ -7,8 +7,13 @@ export default function Notetaker() {
   const [responseData, setResponseData] = useState("Result appears here");
 
   return (
-    <div>
-      <h1>Notetaker</h1>
+    <div className="flex flex-col w-full h-full justify-center items-center p-8">
+      <h1
+        className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-7xl font-semibold text-transparent"
+        data-aos="fade-up"
+      >
+        Notetaker
+      </h1>
       <InitForm postUrl="/api/summarizer" setLoadedData={setResponseData} />
       <p>{responseData}</p>
     </div>
--- components/initForm.tsx
+++ components/initForm.tsx
@@ -40,10 +40,31 @@ export default function InitForm(props: InitFormProps) {
         const response = await onSubmit(event, props);
         props.setLoadedData(await response.text());
       }}
+      className="flex flex-col w-full h-full p-8 gap-8 justify-center items-stretch"
     >
-      <label htmlFor="initialInput">Input Here</label>
-      <input id="initialInput" name="initialInput" type="text"></input>
-      <button type="submit">Submit</button>
+      <label
+        htmlFor="initialInput"
+        className="text-center text-2xl text-indigo-200/65"
+        data-aos="fade-up"
+        data-aos-delay={200}
+      >
+        Enter your lecture slides here
+      </label>
+      <input
+        id="initialInput"
+        name="initialInput"
+        type="text"
+        height="4"
+        className="border-2 p-2 bg-indigo-950/40 border-indigo-800 h-full"
+      ></input>
+      <span className="btn-sm relative rounded-full bg-gray-800/40 px-2.5 py-0.5 text-xs font-normal before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_bottom,theme(colors.gray.700/.15),theme(colors.gray.700/.5))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-gray-800/60">
+        <button
+          type="submit"
+          className="border-2 rounded-xl p-2 bg-indigo-950/40 border-gray-500 text-xl"
+        >
+          Generate Notes
+        </button>
+      </span>
     </form>
   );
 }
